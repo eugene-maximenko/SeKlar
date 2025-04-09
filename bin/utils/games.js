28d51e277f9ac4d6f231cffe60c784bd2a001b0f
Eugene Maximenko <zhek4770@gmail.com>
2025-04-09 11:52:02 +0200

    Add life events list

--- bin/utils/games.js
+++ bin/utils/games.js
@@ -1,3 +1,48 @@
+const financialEvents = [
+        // 🟢 Income
+        {
+          type: "income",
+          description: "Your friend paid back an old debt"
+        },
+        {
+          type: "income",
+          description: "You received a tax refund"
+        },
+        {
+          type: "income",
+          description: "Found cash in your winter coat"
+        },
+        {
+          type: "income",
+          description: "Got a surprise work bonus"
+        },
+        {
+          type: "income",
+          description: "Sold something you no longer need"
+        },      
+        // 🔴 Cost
+        {
+          type: "cost",
+          description: "You bought new furniture"
+        },
+        {
+          type: "cost",
+          description: "Paid an unexpected medical bill"
+        },
+        {
+          type: "cost",
+          description: "Your car broke down and needed repairs"
+        },
+        {
+          type: "cost",
+          description: "You went on a spontaneous weekend trip"
+        },
+        {
+          type: "cost",
+          description: "Accidentally dropped your phone and had to replace it"
+        }
+      ];
+
 const updateInteractiveSection = () => {
     
     return "Don`t quit! Keep it up!"
