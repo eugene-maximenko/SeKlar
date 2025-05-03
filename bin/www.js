d6f5ce5cd2716611913378a69e52833399c83c76
Eugene Maximenko <zhek4770@gmail.com>
2025-05-03 22:50:28 +0200

    Add skip button

---
+++ bin/www.js
@@ -0,0 +1,119 @@
+const app = require('../app');
+const http = require('http');
+const socketio = require('socket.io');
+
+// Import helpers
+const { pickRandomFinancialEvent, pickRandomStockMarketCard, } = require('./utils/cards')
+const { addUser, updateStateDelta, applyStateDelta, updateStock, prepareStockState, approveStockOperation } = require('./utils/users')
+
+// Configure port
+const port = process.env.PORT || '3001'
+app.set('port', port);
+
+// Configure server
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+io.on('connection', (socket) => {
+
+  let stockCompanyName = null
+  let actualStockPrice = null
+
+  // Game starts
+  socket.on('user:join', () => {
+
+    console.log(`User ${socket.id} opened the game page`)
+
+    const user = addUser(socket.id)
+
+    // Display change in cash
+    socket.emit('cash:update', user)
+  })
+
+  // 1st card
+  socket.on('game:start', () => {
+
+    const randomFinancialEvent = pickRandomFinancialEvent()
+
+    updateStateDelta(socket.id, randomFinancialEvent.amount)
+
+    // Display randomEventCard
+    socket.emit("randomEventCard:display", randomFinancialEvent)
+  })
+
+  // Apply changes after random event card
+  socket.on('state:approveUpdate', () => {
+
+    const user = applyStateDelta(socket.id)
+
+    // Display change in cash
+    socket.emit('cash:update', user)
+
+    const randomStockCard = pickRandomStockMarketCard()
+
+    // Update values of mediator variables
+    stockCompanyName = randomStockCard.companyName
+    actualStockPrice = randomStockCard.actualPrice
+
+    // Send stock market card to a client
+    socket.emit('stockMarketCard:display',
+      randomStockCard
+    )
+  })
+
+
+
+  // Logic for purhcase / sell of stocks  
+  socket.on('state:stockUpdate', ({ amount, operationType }) => {
+
+    // Logs
+    console.log('We got state:stockUpdate event from client :)');
+    console.log(amount, operationType);
+
+    // Approve stock operations 
+    const stockOperationIsApproved = approveStockOperation({ amount, id: socket.id, operationType, actualStockPrice, stockCompanyName })
+
+    if (stockOperationIsApproved) {
+
+      // Update cash
+      const cashAmountDelta = -updateStock({ amount, operationType, id: socket.id, stockCompanyName, actualStockPrice })
+      updateStateDelta(socket.id, cashAmountDelta)
+      const user = applyStateDelta(socket.id)
+
+      // Display change in cash
+      socket.emit('cash:update', user)
+
+      // Display new state for stock in `Assets` section
+      const stockState = prepareStockState(socket.id)
+      socket.emit('state:stockDisplay', stockState)
+
+      const randomStockCard = pickRandomStockMarketCard()
+
+      if (randomStockCard.companyName in stockState) {
+        randomStockCard.amountOnHands = stockState[randomStockCard.companyName].amount
+        randomStockCard.averagePrice = stockState[randomStockCard.companyName].averagePrice
+      } else {
+        randomStockCard.amountOnHands = 0
+      }
+
+      stockCompanyName = randomStockCard.companyName
+      actualStockPrice = randomStockCard.actualPrice
+
+      socket.emit('stockMarketCard:display',
+        randomStockCard)
+    } else {
+      socket.emit('notification:operationIsNotApproved')
+    }
+  }
+  )
+
+  socket.on('skipTheCard', () => {
+    console.log('User skips the current card');
+  })
+
+
+  // Closing brackets to the io function
+})
+
+server.listen(port);
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -23,59 +23,59 @@ startButton.addEventListener('click', () => {
 socket.on("randomEventCard:display", randomFinancialEvent => {
 
     console.log(JSON.stringify(randomFinancialEvent))
-    
+
     const amountIsPositiveNumber = randomFinancialEvent.amount > 0 ? true : false
-    
+
     const html = Mustache.render(cashEventCardTemplate, {
         eventDescription: randomFinancialEvent.description,
         amount: amountIsPositiveNumber ? `+${randomFinancialEvent.amount}` : randomFinancialEvent.amount
     });
     interactiveSection.innerHTML = html
-    
+
     let cashEventColor = document.getElementById("cash-event-amount").style
-    
+
     if (randomFinancialEvent.amount > 0) {
         cashEventColor.color = "#00d062"
-    } else {cashEventColor.color = "red"}
-    
+    } else { cashEventColor.color = "red" }
+
     const nextCardButton = document.querySelector('#next-card-button')
-    
+
     nextCardButton.addEventListener('click', () => {
         socket.emit('state:approveUpdate')
     })
 })
 
-// Display cash
-socket.on('state:display', user => {
+// Display change in cash
+socket.on('cash:update', user => {
     console.log(JSON.stringify(user))
 
-    headerCashPointer.style.opacity = 0.1    
+    headerCashPointer.style.opacity = 0.1
     assetsCashPointer.style.opacity = 0.1
-    
+
     setTimeout(() => {
         headerCashPointer.innerText = user.cashAmount
         assetsCashPointer.innerText = user.cashAmount
-        
+
         headerCashPointer.style.opacity = 1;
         assetsCashPointer.style.opacity = 1
-      }, 500);  // 1000ms matches the duration of the fade-out
+    }, 500);  // 1000ms matches the duration of the fade-out
 })
 
 // Stock Card
-socket.on('stockMarketCard:display', ({actualPrice,
+socket.on('stockMarketCard:display', ({ actualPrice,
     companyName,
-    fairPrice, amountOnHands, averagePrice}) => {
-    
+    fairPrice, amountOnHands, averagePrice }) => {
+
     const stockAmountOnHands = amountOnHands > 0 ? `${amountOnHands} по ${averagePrice}` : 0
 
     const html = Mustache.render(stockMarketCardTemplate, {
-    actualPrice, companyName, fairPrice, stockAmountOnHands  
+        actualPrice, companyName, fairPrice, stockAmountOnHands
     });
     interactiveSection.innerHTML = html
 
     const inputElement = document.querySelector('#input_amount')
     const amountSum = document.querySelector(".amount-sum")
-    
+
     // Handler for the amount field
     inputElement.onchange = function () {
         amountSum.innerText = this.value * actualPrice
@@ -84,22 +84,21 @@ socket.on('stockMarketCard:display', ({actualPrice,
     // Validator for input
     inputElement.addEventListener('blur', () => {
         const value = inputElement.value.trim();
-      
+
         // Allow only digits (and not empty string)
         const isValid = /^[1-9][0-9]*$/.test(value);
-      
+
         if (!isValid) {
-          // Optional: show error, shake input, or reset value
-          alert('Only positive numbers allowed!');
-          // reset input area
-          inputElement.value = 1
-          amountSum.innerText = actualPrice
+            // Optional: show error, shake input, or reset value
+            alert('Only positive numbers allowed!');
+            // reset input area
+            inputElement.value = 1
+            amountSum.innerText = actualPrice
         }
-      });
-      
+    });
+
 
     // Handler for active lable
-
     let operationType = 'buySelect';
 
     document.querySelector('.tabs-holder').addEventListener('change', function (e) {
@@ -109,38 +108,45 @@ socket.on('stockMarketCard:display', ({actualPrice,
     // Next button handler
     const nextCardButton = document.querySelector('#next-card-button')
     nextCardButton.addEventListener('click', () => {
-        socket.emit('state:stockUpdate', {amount: Number(inputElement.value) || 1, operationType})
+        socket.emit('state:stockUpdate', { amount: Number(inputElement.value) || 1, operationType })
+    })
+
+    // Skip button handler
+    const skipButton = document.querySelector('#skip-button')
+    skipButton.addEventListener('click', () => {
+        socket.emit('skipTheCard')
     })
 })
 
 // Display Stock State
 socket.on("state:stockDisplay", stockState => {
 
-    console.log(`Client got this stockState `+JSON.stringify(stockState))
-    
-    const stockRows = document.querySelectorAll(`#stock-row`)
-    
-    stockRows.forEach((e)=>{e.remove()})
+    console.log(`Client got this stockState ` + JSON.stringify(stockState))
 
-    for(key in stockState) {
-        
-        const html = Mustache.render(stockStateTemplate,{companyName: key, 
+    const stockRows = document.querySelectorAll(`#stock-row`)
+
+    stockRows.forEach((e) => { e.remove() })
+
+    for (key in stockState) {
+
+        const html = Mustache.render(stockStateTemplate, {
+            companyName: key,
             amount: stockState[key].amount,
             totalInvestment: stockState[key].totalInvestment,
             averagePrice: stockState[key].averagePrice,
         });
-        
-            stockStateSection.insertAdjacentHTML('afterend', html)
 
-        }
-        
-        const nextCardButton = document.querySelector('#next-card-button')
-        
-        nextCardButton.addEventListener('click', () => {
-            socket.emit('state:stockUpdate', {amount: inputElement.value || 1, operationType})
-        })
-}) 
+        stockStateSection.insertAdjacentHTML('afterend', html)
 
-socket.on('notification:operationIsNotApproved', ()=>{
+    }
+
+    const nextCardButton = document.querySelector('#next-card-button')
+
+    nextCardButton.addEventListener('click', () => {
+        socket.emit('state:stockUpdate', { amount: inputElement.value || 1, operationType })
+    })
+})
+
+socket.on('notification:operationIsNotApproved', () => {
     alert('Operation is not approved, try again :)')
 })
